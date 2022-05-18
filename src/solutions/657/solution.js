330cb10c3408c4da3f3ce464ebfd611e8a66a43b
seungjaey <seungjaey@gmail.com>
2022-05-18 17:56:45 +0900

    leetcode 657 v2

--- src/solutions/657/solution.js
+++ src/solutions/657/solution.js
@@ -1,6 +1,19 @@
 // NOTE: 문제 링크 = https://leetcode.com/problems/robot-return-to-origin/
+/* V2
+function getMatchedCount(s, regex) {
+  return [...s.matchAll(regex)].length;
+}
 
-// moveLength 2 * 10^4
+function solution(moves) {
+  const length = moves.length;
+  const isOdd = length % 2 !== 0;
+  if (isOdd) {
+    return false;
+  }
+  return (getMatchedCount(moves, /L/g) === getMatchedCount(moves, /R/g))
+    && (getMatchedCount(moves, /U/g) === getMatchedCount(moves, /D/g))
+}
+*/
 function solution(moves) {
   const length = moves.length;
   const isOdd = length % 2 !== 0;
