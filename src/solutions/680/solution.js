f99d31c5416b7489e52eed3fcc099d4d0b99cce8
seungjaey <seungjaey@gmail.com>
2022-05-18 15:59:37 +0900

    leetcode 680

--- src/solutions/680/solution.js
+++ src/solutions/680/solution.js
@@ -1,10 +1,33 @@
-// NOTE: 문제 링크 = 
-
-function solution() {
-
+// NOTE: 문제 링크 = https://leetcode.com/problems/valid-palindrome-ii/
+function checkPalindrome(s, i , j) {
+  while (i < j) {
+    if (s.charAt(i) !== s.charAt(j)) {
+      return false;
+    }
+    i += 1;
+    j -= 1;
+  }
+  return true;
 }
 
+function solution(s) {
+  let i = 0;
+  let j = s.length - 1;
+
+  while (i < j) {
+    if (s.charAt(i) !== s.charAt(j)) {
+      return (checkPalindrome(s, i, j - 1))
+        || (checkPalindrome(s, i + 1, j))
+    }
+    i += 1;
+    j -= 1;
+  }
+  return true;
+}
+
+
+
 // 테스트 케이스
-console.log(solution());
-console.log(solution());
-console.log(solution());
+console.log(solution("aba")); // true
+console.log(solution("abca")); // true
+console.log(solution("abc")); // false
