7b7145bb34f4347d87829f24b02b12b777b0af1b
seungjaey <seungjaey@gmail.com>
2022-06-16 16:23:10 +0900

    leetcode 541

---
+++ src/solutions/541/solution.js
@@ -0,0 +1,22 @@
+// NOTE: 문제 링크 = https://leetcode.com/problems/reverse-string-ii/
+
+function solution(s, k) {
+  const l = s.length;
+
+  let i = 0;
+  while (i < k) {
+
+  }
+}
+
+// 테스트 케이스
+console.log(solution("abcdefg", 2)); // bacdfeg
+console.log(solution("abcd", 2)); // bacd
+/*
+abcd
+
+1 bcd a
+2 cd ab
+3 d abc
+4  abcd
+ */
--- src/solutions/645/solution.js
+++ src/solutions/645/solution.js
@@ -1,25 +1,24 @@
 // NOTE: 문제 링크 = https://leetcode.com/problems/set-mismatch/
 
 function solution(nums) {
-  nums = nums.sort((a, b) => a - b);
-  const length = nums.length;
-  const arr = Array.from({ length }, (e, i) => {
-    return i + 1;
-  });
+  nums.sort((a, b) => a - b);
+  const l = nums.length;
+  const res = new Set();
+  const list = Array.from(
+    { length: l },
+    (e, i) => i + 1,
+  );
+  console.log(list);
+  console.log(nums);
 
-  for (let i = 0; i < length; i += 1) {
-    if (arr[i] === nums[i]) {}
-
-  }
-
+  return res;
 }
 
 // 테스트 케이스
 // console.log(solution([1,2,2,4])); // [2,3]
 // console.log(solution([1,1])); // [1,2]
-console.log(solution([2,2])); // [2,1]
+// console.log(solution([2,2])); // [2,1]
 // console.log(solution([3,2,2])); // [2, 1]
-// console.log(solution([3,2,3,4,6,5])); // [3,1]
-console.log(solution([1,5,3,2,2,7,6,4,8,9])); // [2,10]
-1,2, 2,3,4,5,6,7,8,9
+console.log(solution([3,2,3,4,6,5])); // [3,1]
+// console.log(solution([1,5,3,2,2,7,6,4,8,9])); // [2,10]
 //  node ./src/solutions/645/solution.js
